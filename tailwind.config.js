1f4351f6e4048e11824dc22cc35cb1d38d188ce6
Michael Rodriguez <168235723+MichaelDriguez@users.noreply.github.com>
2024-08-13 14:17:59 -0500

    Check if Tailwind works

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'beige': '#DAD7CD',
+        'green-1': '#A3B18A',
+        'green-2': '#588157',
+        'green-3': '#3A5A40',
+        'green-4': '#344E41',
+      },
+    },
+  },
+  plugins: [],
+}
+
